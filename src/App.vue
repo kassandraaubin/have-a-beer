7d5bd5fb19f2561ac6fb3059b5cadd749585da9b
Kass <56520362+kassandraaubin@users.noreply.github.com>
2022-02-25 11:28:06 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,150 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div id="app">
+  <section>
+    <h1>🍺 Make yourself some Beers 🍻</h1>
+    <div v-if="!beers.length" class="loading">Loading...</div>
+    <div v-for="beer in beers" :key="beer" class="beer-contain">
+      <div class="beer-img">
+        <img :src="beer.img" height="350" />
+      </div>
+      <div class="beer-info">
+        <h2>{{ beer.name }}</h2>
+        <p class="bright">{{ beer.tagline }}</p>
+        <p><span class="bright">Description:</span> {{ beer.desc }}</p>
+        <p><span class="bright">Tips:</span> {{ beer.tips }}</p>
+        <h3 class="bright">Food Pairings</h3>
+        <ul>
+          <li v-for="item in beer.food" :key="item">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      bottom: false,
+      beers: []
+    };
+  },
+  watch: {
+    bottom(newValue){
+      if(newValue){
+        this.addBeer();
+      }
+    }
+  },
+  created(){
+    window.addEventListener("scroll", () => {
+      this.bottom = this.bottomVisible()
+    });
+    this.addBeer();
+  },
+  methods: {
+    bottomVisible(){
+      const scrollY = window.scrollY;
+      const visible = document.documentElement.clientHeight;
+      const pageHeight = document.documentElement.scrollHeight;
+      const bottomOfPage = visible + scrollY >= pageHeight
+      return bottomOfPage || pageHeight < visible;
+    },
+    addBeer(){
+      axios.get("https://api.punkapi.com/v2/beers/random").then((response) => {
+        let api = response.data[0];
+        let apiInfo = {
+          name: api.name,
+          desc: api.description,
+          img: api.image_url,
+          tips: api.brewers_tips,
+          tagline: api.tagline,
+          food: api.food_pairing
+        };
+        this.beers.push(apiInfo);
+        if(this.bottomVisible()){
+          this.addBeer();
+        }
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  font-family: 'Archivo Narrow', sans-serif;
+  background: #25859a;
+}
+
+h1, h2, h3, h4 {
+  font-family: 'Fjalla One', sans-serif;
+}
+
+h1 {
+  margin-top: 40px;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.loading {
+  color: white;
+  text-align: center;
+  font-size: 20px;
+}
+
+.display {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  flex-direction: column;
+}
+
+.beer-contain {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  width: 50%;
+  margin: 20px 24%;
+  box-shadow: 0 2px 3px 1px rgba(30, 0, 0, 0.1);
+}
+
+.beer-img {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  padding: 30px;
+  background: #FF6542;
+  border: 1px solid #88498F;
+  border-right: 1px solid #f44822;
+}
+
+.beer-info {
+  background: #564154;
+  color: white;
+  padding: 30px;
+  border: 1px solid #88498F;
+}
+.bright {
+  color: #fcd7cf;
+  font-family: 'Contrail One', sans-serif;
+}
+
+h3 {
+  margin-bottom: 5px;
+}
+
+ul {
+  margin-top: 5px;
 }
 </style>
